0df1a8c1b0127b30548a4fc085f5de27dcd22c52
Anastasiia <anastasiia.kuropatkina@Anastasiias-MBP.lan>
2024-02-23 17:28:56 +0100

    add division support

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sys
 
 parser = argparse.ArgumentParser(description="Simple CLI Calculator")
 parser.add_argument('num1', type=int)
-parser.add_argument('symbol', choices=['+', '-'])
+parser.add_argument('symbol', choices=['+', '-', '/'])
 parser.add_argument('num2', type=int)
 args = parser.parse_args()
 
@@ -12,6 +12,8 @@ if args.symbol == '+':
     result = args.num1 + args.num2
 elif args.symbol == '-':
     result = args.num1 - args.num2
+elif args.symbol == '/':
+    result = args.num1 / args.num2
 else:
     print("Unsupported operation")
     sys.exit(1)
